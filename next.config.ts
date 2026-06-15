32f559c1def4a464481d76cc296a610f36cb6d71
Mario Sbardella <mariosbardella@protonmail.com>
2026-06-15 07:47:28 -0400

    Include Swiss Ephemeris data in deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ["swisseph"],
+  outputFileTracingIncludes: {
+    "/api/astrology/*": ["./node_modules/swisseph/ephe/**/*"],
+  },
   trailingSlash: true,
   async redirects() {
     return [
